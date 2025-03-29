fe7cfb2151881350c4ba042c0f239473b6c23ec1
ashensavi <ashensavinda0@gmail.com>
2025-03-29 11:05:09 +0530

    Initialized the backend

---
+++ src/main/java/edu/icet/ecom/entity/UserEntity.java
@@ -0,0 +1,18 @@
+package edu.icet.ecom.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String password;
+    private String name;
+    private String phone;
+    private String role;
+}
