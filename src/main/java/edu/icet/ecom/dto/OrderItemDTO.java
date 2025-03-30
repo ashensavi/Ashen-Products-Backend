b3b3e0988910fb7c08d1f0c0d5f29b7448584c73
ashensavi <ashensavinda0@gmail.com>
2025-03-30 10:53:34 +0530

    updated dtos and entities with proper annotations & validations

--- src/main/java/edu/icet/ecom/dto/OrderItemDTO.java
+++ src/main/java/edu/icet/ecom/dto/OrderItemDTO.java
@@ -1,9 +1,26 @@
 package edu.icet.ecom.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemDTO {
     private Long id;
+
+    @NotNull(message = "Order ID is required")
     private Long orderId;
+
+    @NotNull(message = "Product ID is required")
     private Long productId;
+
+    @Min(value = 1, message = "Quantity must be at least 1")
     private int quantity;
+
+    @Min(value = 0, message = "Price must be non-negative")
     private double price;
 }
--- src/main/java/edu/icet/ecom/entity/UserEntity.java
+++ src/main/java/edu/icet/ecom/entity/UserEntity.java
@@ -1,10 +1,14 @@
 package edu.icet.ecom.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
 @Entity
 public class UserEntity {
     @Id
